7ab8449518f8e00901d9e0c4ebfd7150ee9602c2
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-01 17:45:03 -0400

    Added conditional to end program if cancel clicked

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// MY CODE FOLLOWS:
+
+function generatePassword() {
+  // Initiate numChar with input- this variable will hold the numbers of characters in the password.
+  let numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
+
+  // If the user clicks cancel, end the program.
+  if (numChar === null) {
+    return;
+  }
+
+  // Validates that the password is a number between 8 and 128, inclusive; otherwise alerts with error and prompts for length again
+  while (numChar < 8 || numChar > 128 || isNaN(numChar)) {
+    alert("You must choose a number between 8 and 128.");
+    numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
+    if (numChar === null) return;
+  }
+
+}
