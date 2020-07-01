aed7a5e6993a02fd73d3d30459040ec21635a35f
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-01 17:52:21 -0400

    Added prompts for character types allowed in PW

--- script.js
+++ script.js
@@ -20,9 +20,7 @@ function generatePassword() {
   let numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
 
   // If the user clicks cancel, end the program.
-  if (numChar === null) {
-    return;
-  }
+  if (numChar === null) return;
 
   // Validates that the password is a number between 8 and 128, inclusive; otherwise alerts with error and prompts for length again
   while (numChar < 8 || numChar > 128 || isNaN(numChar)) {
@@ -30,5 +28,13 @@ function generatePassword() {
     numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
     if (numChar === null) return;
   }
+
+  // Ask user whether or not they want to include lowercase letters, uppercase letters, numbers, and/or special chaarcters.
+  const includeLower = confirm("Do you want to include lowercase letters?");
+  const includeUpper = confirm("Do you want to include UPPERCASE letters?");
+  const includeNumbers = confirm("Do you want to include numbers?");
+  const includeSpecChar = confirm("Do you want to include special characters (e.g. $, *, >, etc)?");
+
+
 
 }
