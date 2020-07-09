af347012e10655edc9dba2bd0673bbac1d180626
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-09 19:15:26 -0400

    Added files for PW generator w/o popups

--- README.md
+++ README.md
@@ -1,34 +1,6 @@
-# Password_Generator
-HW3 for UNH Coding Bootcamp: Use JavaScript to create an application that takes in parameters and generates a random password based on those parameters.
-
-## Description
-
-With this project, I was given the HTML and CSS for a web app to generate a random password as well as a starter JavaScript file with the code for making the button in the app run the function. (See second commit for the starter code with which I was provided: [Added starter code for the assignment](https://github.com/LivesInRoom29/Password_Generator/commit/4bca5aa01390c260f79f8bde95a25af56f61ec16)).
-
-I wrote the program that would take in certain parameters from the user and output the random password. The user is able choose the length of the password (between 8 and 128, inclusive) and what types of charcters to use (lowercase, uppercase, numbers, and/or special characters).
-
-
-[Password Generator](https://github.com/LivesInRoom29/Password_Generator)
-
-![pwGenerator_screenshot](https://user-images.githubusercontent.com/61219066/86302368-75161b80-bbd6-11ea-8a77-5559e0ca4ee5.jpg)
-
-
-## Learning Outcomes
-
-* JavaScript
-  * Became more familiar with JS **syntax**.
-  * Gained experience writing **functions** and using JS **methods**.
-  * Gained experience with finding appropriate JS methods to use, such as .join(), .push(), etc.
-  * Gained experience in troubleshooting functions and programs using the browser **console**.
+# Password Generator w/ Check Boxes
+A random password generator that utilizes text input and check boxes for options instead of popups.
 
 
 ## Credits
-
-* The code that I used to generate a random number within a specific range was adapted from a [GeeksforGeeks](https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/) article.
-
-* Thanks to the UNH Bootcamp instructor, TAs, and tutors for providing the class with lots of practice using JavaScript
-
-* Thanks as well to my classmates for sharing their questions in class and during office hours.
-
-* Lastly, as always. thanks to all the wonderful folks who post their questions and answers all over the web - you are life savers!
-
+Image by <a href="https://pixabay.com/users/TheDigitalArtist-202249/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3400657">Pete Linforth</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3400657">Pixabay</a>
--- pwgen.js
+++ pwgen.js
@@ -1,7 +1,7 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Creates variable attached to the "Generate PW" button
+var generateBtn = document.querySelector("#generatepw");
 
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,7 +10,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // MY CODE FOLLOWS:
@@ -19,44 +19,41 @@ generateBtn.addEventListener("click", writePassword);
 let possibleCharArray = [];
 
 function generatePassword() {
+
   // Initiate numChar with input- this variable will hold the numbers of characters in the password.
-  let numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
+  let numChar = document.getElementById("numChar").value;
 
   // If the user clicks cancel, end the program.
   if (numChar === null) return;
 
-  // Validates that the password is an integer number between 8 and 128, inclusive; otherwise alerts with error and prompts for length again
-  while (numChar < 8 || numChar > 128 || isNaN(numChar) || numChar % 1 !== 0) {
+  // Validates that the password is a an integer number between 8 and 128, inclusive; otherwise alerts with error and prompts for length again
+  if (numChar < 8 || numChar > 128 || isNaN(numChar) || numChar % 1 !== 0) {
     alert("You must choose a number between 8 and 128.");
-    numChar = prompt("How many characters do you want in the password? (Must be at least 8 and no more than 128.)");
-    // If the user clicks cancel while within this loop, ends the program.
-    if (numChar === null) return;
+    return;
   }
 
-  // Ask the user whether they want to include lowercase letters.
-  const includeLower = confirm("Do you want to include lowercase letters?");
-  // If yes, add lowercase letters to the array off possible characters (the numbers 97 - 122 are ascii values for lowercase letters).
+  // Check to see whether the check boxes are checked for the 4 different options.
+  var includeLower = document.getElementById("lowercase").checked;
+  var includeUpper = document.getElementById("uppercase").checked;
+  var includeNumbers = document.getElementById("numbers").checked;
+  var includeSpecChar = document.getElementById("spechar").checked;
+
+  // If lowercase check box checked, add lowercase letters to the array off possible characters (the numbers 97 - 122 are ascii values for lowercase letters).
   if (includeLower) {
     asciiToCharArray(97, 122);
   }
 
-  // Ask the user whether they want to include uppercase letters.
-  const includeUpper = confirm("Do you want to include UPPERCASE letters?");
-  // If yes, add uppercase letters to the array of possible characters (ascii 65-90)
+  // If uppercase check box is checked, add uppercase letters to the array of possible characters (ascii 65-90)
   if (includeUpper) {
     asciiToCharArray(65, 90);
   }
 
-  // Ask the user whether they want to include numbers.
-  const includeNumbers = confirm("Do you want to include numbers?");
-  // If yes, add numbers to the array of possible characters (ascii 48-57)
+  // If numbers box is checked, add numbers to the array of possible characters (ascii 48-57)
   if (includeNumbers) {
     asciiToCharArray(48, 57);
   }
 
-  // Ask the user whether they want to include special charcters.
-  const includeSpecChar = confirm("Do you want to include special characters (e.g. $, *, >, etc)?");
-  // If yes, add special characters to the array of possible characters (33-47, 58-64, 91-96, 123-126)
+  // If special characters box is checkec, add special characters to the array of possible characters (33-47, 58-64, 91-96, 123-126)
   if (includeSpecChar) {
     asciiToCharArray(33, 47);
     asciiToCharArray(58, 64);
@@ -82,7 +79,7 @@ function generatePassword() {
   // Create a string from the PW array with join.
   const pwString = pwArray.join("")
 
-  //Clear the array of possible characters in case the user hits the "Generate Password again.
+  //Clear the array of possible characters in case the user hits the "Generate Password" again.
   possibleCharArray = [];
 
   // Return the password string
